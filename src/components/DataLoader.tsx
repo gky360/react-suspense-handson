ff76c8637134265ff3e35ccbb7eac28475b80e2e
gky360 <gky360@gmail.com>
2022-01-23 15:28:19 +0900

    Use hook

--- src/components/DataLoader.tsx
+++ src/components/DataLoader.tsx
@@ -2,14 +2,17 @@ import { fetchData1 } from '../utils';
 
 let data: string | undefined;
 
-export interface DataLoaderProps {}
-
-export const DataLoader = ({}: DataLoaderProps): JSX.Element | null => {
-  // dataがまだ無ければローディングを開始する
+const useData1 = (): string => {
   if (data === undefined) {
     throw fetchData1().then((d) => (data = d));
   }
-  // データがあればそれを表示
+  return data;
+};
+
+export interface DataLoaderProps {}
+
+export const DataLoader = ({}: DataLoaderProps): JSX.Element | null => {
+  const data = useData1();
   return (
     <div>
       <div>Data is {data}</div>
